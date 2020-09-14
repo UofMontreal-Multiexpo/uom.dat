5dc3df763ffde44cbff5f00500599f4c481baa0c
Gauthier_Magnin <gauthier.magnin@gmail.com>
2020-09-14 16:50:18 +0200

    feat: Add a function to plot the MCR scatter plot
    Add one function and add ggplot2 to the requested packages

--- R/mixer.R
+++ R/mixer.R
@@ -820,3 +820,223 @@ thq_freq_by_group = function(values = NULL, references = NULL,
 }
 
 
+
+#### Maximum Cumulative Ratio - summary functions ####
+
+#' MCR approach scatter plot
+#' 
+#' Plot \mjeqn{log_{10}(HI)}{log10(HI)} versus \mjeqn{log_{10}(MCR - 1)}{log10(MCR - 1)} with the
+#'  percentage of the reciprocal of the maximum cumulative ratio, the elements producing the top hazard
+#'  quotients and the associated MIAT groups.
+#' 
+#' @details
+#' The chart being plotted with the `ggplot2` package, it can be modified or completed afterwards using
+#'  [`ggplot2::last_plot`] or the returned chart.
+#' 
+#' Color specification can be done using the R predefined color names or hexadecimal values.
+#'  
+#' Arguments `values` and `references` are used to compute the hazard quotients and the hazard indexes
+#'  before searching for the top and maximum hazard quotients, computing the maximum cumulative ratios,
+#'  performing the classification then plot the chart. Thus, call the function with the arguments `hi`,
+#'  `mcr`, `thq` and `groups` is faster (if they are already computed).
+#' 
+#' \loadmathjax
+#' The mixtures are assigned to the groups according the following conditions:
+#' * Group I: \mjeqn{MHQ_i \ge 1}{MHQ_i >= 1}
+#' * Group II: \mjeqn{MHQ_i < 1, HI_i \le 1}{MHQ_i < 1, HI_i <= 1}
+#' * Group IIIA: \mjeqn{MHQ_i < 1, HI_i > 1, MCR_i < 2}{MHQ_i < 1, HI_i > 1, MCR_i < 2}
+#' * Group IIIB: \mjeqn{MHQ_i < 1, HI_i > 1, MCR_i \ge 2}{MHQ_i < 1, HI_i > 1, MCR_i >= 2}
+#' 
+#' The maximum cumulative ratio of the vector \eqn{i} is given by:
+#'  \mjdeqn{MCR_i = \frac{HI_i}{MHQ_i}}{MCR_i = HI_i / MHQ_i}
+#'  where \eqn{HI} denotes the hazard index and \eqn{MHQ} denotes the maximum hazard quotient.
+#'  
+#' The reciprocal of the maximum cumulative ratio of the vector \eqn{i} is given by:
+#'  \mjdeqn{Reciprocal of MCR_i = \frac{1}{MCR_i} = \frac{MHQ_i}{HI_i}}{Reciprocal of MCR_i = 1 / MCR_i = MHQ_i / HI_i}
+#'  where \eqn{MCR}, \eqn{MHQ} and {HI} denotes the maximum cumulative ratio, the maximum hazard
+#'  quotient and the hazard index respectively.
+#' 
+#' The hazard index of the vector \eqn{i} is given by:
+#'  \mjdeqn{HI_i = \sum_{j = 1}^N HQ_{i,j}}{HI_i = sum(HQ_ij) from j = 1 to N}
+#'  where \eqn{HQ} denotes the hazard quotients and \eqn{N} denotes the number of hazard quotients.
+#'  
+#' The maximum hazard quotient of the vector \eqn{i} is given by:
+#'  \mjdeqn{MHQ_i = HQ_{M,i} = \max_{j \in \lbrace 1,...,N\rbrace} HQ_{i,j}}{MHQ_i = HQ_Mi = max HQ_i}
+#'  where \eqn{HQ} denotes the hazard quotients and \eqn{N} denotes the number of hazard quotients.
+#' 
+#' The hazard quotient of the value \eqn{j} in the vector \eqn{i} is given by:
+#'  \mjdeqn{HQ_{i,j} = \frac{V_{i,j}}{RV_j}}{HQ_ij = V_ij / RV_j}
+#'  where \eqn{V} denotes the `values` and \eqn{RV} denotes the `references`.
+#' 
+#' @note
+#' Due to the multiple possible usages, all arguments except `values` and `references` must be explicitly
+#'  named in the function call.
+#' 
+#' @usage
+#' mcr_chart(values, references,
+#'           thq_col = NULL, regions = FALSE,
+#'           regions_col = c("#b3cde3", "#edf8fb", "#8c96c6", "#88419d"), regions_alpha = 0.2,
+#'           regions_lab = !regions, regression = FALSE)
+#' mcr_chart(hi, mcr, thq, groups,
+#'           thq_col = NULL, regions = FALSE,
+#'           regions_col = c("#b3cde3", "#edf8fb", "#8c96c6", "#88419d"), regions_alpha = 0.2,
+#'           regions_lab = !regions, regression = FALSE)
+#' @param values Numeric named matrix. Vectors of values for which the chart is to be plotted.
+#' @param references Numeric vector. Reference values associated with the `values`.
+#' @param hi Numeric vector. **H**azard **i**ndexes for which the chart is to be plotted.
+#' @param mcr Numeric vector. **M**aximum **c**umulative **r**atios associated with the hazard indexes
+#'  `hi`.
+#' @param thq Numeric named vector or list of numeric named vectors. **T**op **h**azard **q**uotients
+#'  associated with the hazard indexes `hi`. If list, only the first named value of each element of the
+#'  list is considered.
+#' @param groups Character vector. MIAT groups associated with the hazard indexes `hi`.
+#' @param thq_col Character named vector. Colors to assign to the **t**op **h**azard **q**uotients
+#'  elements.
+#' @param regions If `TRUE`, the regions corresponding to the MIAT groups are filled with the colors
+#'  defining by `regions_col`.
+#' @param regions_col Character vector of length 4. Define the colors for the regions of the MIAT groups
+#'  (in order: I, II, IIIA and IIIB).
+#' @param regions_alpha Value between 0 and 1. Transparency of the regions filled with `regions_col`.
+#' @param regios_lab Logical value or vector of length 4. Define if labels for the MIAT groups should
+#'  be displayed (in order: I, II, IIIA, IIIB). `TRUE` and `FALSE` are special values for all `TRUE` or
+#'  all `FALSE`.
+#' @param regression If `TRUE`, the linear regression between \mjeqn{log_{10}(HI)}{log10(HI)} and
+#'  \mjeqn{log_{10}(MCR - 1)}{log10(MCR - 1)} with 95% confidence interval is represented.
+#' @return Invisible. Chart created with the `ggplot2` package.
+#' 
+#' @author Gauthier Magnin
+#' @references Reyes JM, Price PS (2018).
+#'             An analysis of cumulative risks based on biomonitoring data for six phthalates using the Maximum Cumulative Ratio.
+#'             *Environment International*, 112, 77-84.
+#'             <https://doi.org/10.1016/j.envint.2017.12.008>.
+#' @seealso [`maximum_cumulative_ratio`], [`hazard_index`], [`reciprocal_of_mcr`], [`top_hazard_quotient`].
+#' 
+#' @examples
+#' ## Creating a matrix of 5*50 values and one reference value for each of the 5
+#' ## elements ("a", "b", "c", "d" and "e").
+#' v <- matrix(sample(seq(0.1, 1.1, by = 0.1), 250, replace = TRUE),
+#'             ncol = 50, dimnames = list(letters[1:5]))
+#' r <- sample(seq(1,5), 5, replace = TRUE)
+#' 
+#' mcr_chart(v, r, regions = TRUE)
+#' mcr_chart(v, r,
+#'           thq_col = c(a = "blue", b = "green", c = "red", d = "yellow3", e = "grey"),
+#'           regions = FALSE,
+#'           regions_lab = c(TRUE, TRUE, FALSE, TRUE),
+#'           regression = TRUE)
+#' mcr_chart(hi = hazard_index(v, r),
+#'           mcr = maximum_cumulative_ratio(v, r),
+#'           thq = top_hazard_quotient(v, r),
+#'           groups = classify_mixture(v, r))
+#' 
+#' @md
+#' @export
+mcr_chart = function(values = NULL, references = NULL,
+                     hi = NULL, mcr = NULL, thq = NULL, groups = NULL,
+                     thq_col = NULL, regions = FALSE,
+                     regions_col = c("#b3cde3", "#edf8fb", "#8c96c6", "#88419d"), regions_alpha = 0.2,
+                     regions_lab = !regions, regression = FALSE) {
+  
+  if (is.null(hi)) hi = hazard_index(values, references)
+  if (is.null(mcr)) mcr = maximum_cumulative_ratio(values, references, hi = hi)
+  if (is.null(thq)) thq = top_hazard_quotient(values, references, k = 1)
+  if (is.null(groups)) groups = classify_mixture(hi = hi, mhq = sapply(thq, "[", 1), mcr = mcr)
+  
+  # Récupération des noms des top et calcul des réciproques de mcr
+  thq = sapply(unname(thq), function(v) names(v)[1])
+  rmcr = reciprocal_of_mcr(mcr = mcr)
+  
+  # Préparation des données, fonctions et limites pour le graphique
+  data = data.frame(HI = hi, MCR = mcr, Reciprocal = rmcr, Group = groups, THQ = thq,
+                    x = log10(hi), y = log10(mcr - 1))
+  xlim = c(floor(min(data$x)), ceiling(max(data$x)))
+  ylim = c(floor(min(data$y)), ceiling(max(data$y)))
+  fun.mhq_1 = function(x) log10(10^x - 1)
+  xmin_fun = 0.001
+  xmax_fun = xlim[2] + 1
+  root_fun = uniroot(fun.mhq_1, c(0, 1))$root   # fun.mhq_1(log10(2)) = 0
+  
+  
+  # Initialisation du graphique
+  chart = ggplot2::ggplot(data = data, ggplot2::aes(x = x, y = y))
+  
+  # Coloration des régions
+  if (regions) {
+    # Default colors are colorblind safe and print friendly
+    
+    chart = chart +
+      # A gauche, groupe II
+      ggplot2::geom_polygon(data = data.frame(x = c(-Inf, -Inf, 0, 0),
+                                              y = c(-Inf, Inf, Inf, -Inf)),
+                            ggplot2::aes(fill = "II"),
+                            alpha = regions_alpha) +
+      # A droite, groupe I + délimitation
+      ggplot2::geom_polygon(data = data.frame(x = c(seq(0, xmax_fun, by = 0.001), xmax_fun),
+                                              y = c(-Inf, fun.mhq_1(seq(xmin_fun, xmax_fun, by = 0.001)), -Inf)),
+                            ggplot2::aes(fill = "I", x = x, y = y),
+                            color = "black", linetype = "longdash", alpha = regions_alpha) +
+      # Au centre, groupe IIIA
+      ggplot2::geom_polygon(data = data.frame(x = c(0, seq(0, root_fun, by = 0.001)),
+                                              y = c(0, -Inf, fun.mhq_1(seq(xmin_fun, root_fun, by = 0.001)))),
+                            ggplot2::aes(fill = "IIIA", x = x, y = y),
+                            alpha = regions_alpha) +
+      # En haut, groupe IIIB
+      ggplot2::geom_polygon(data = data.frame(x = c(0, 0, seq(root_fun, xmax_fun, by = 0.001), xmax_fun, xmax_fun),
+                                              y = c(Inf, 0, fun.mhq_1(seq(root_fun, xmax_fun, by = 0.001)), fun.mhq_1(xmax_fun), Inf)),
+                            ggplot2::aes(fill = "IIIB", x = x, y = y),
+                            alpha = regions_alpha) +
+      # Légende associée
+      ggplot2:: scale_fill_manual(values = setNames(regions_col, c("I", "II", "IIIA", "IIIB")),
+                                  name = "MIAT groups")
+  } else {
+    # Courbe délimitant le groupe I
+    chart = chart + ggplot2::stat_function(fun = fun.mhq_1, xlim = c(xmin_fun, xmax_fun),
+                                           color = "black", linetype = "longdash")
+  }
+  
+  # Suite du graphique
+  chart = chart + 
+    ggplot2::geom_point(ggplot2::aes(color = factor(thq))) +
+    # Segment horizontal séparant IIIA et IIIB
+    ggplot2::geom_segment(ggplot2::aes(x = 0, y = 0, xend = root_fun, yend = 0),
+                          color = "black", linetype = "longdash") +
+    # Droite vertical séparant le groupe II du reste
+    ggplot2::geom_vline(xintercept = 0, color = "black", linetype = "longdash") +
+    # Titres des axes et légende
+    ggplot2::labs(x = bquote(log[10]*"(HI)"),
+                  y = bquote(log[10]*"(MCR - 1); MHQ / HI"),
+                  col = "Top Hazard Quotients") +
+    # Ajout de la réciproque de MCR aux labels de l'axe Y
+    ggplot2::scale_y_continuous(labels = function(y) {
+      return(paste0(format(round(y, 1), nsmall = 1), "\n",
+                    format(round(reciprocal_of_mcr(mcr = 10^y + 1) * 100, 1), nsmall = 1), "%"))
+      # 10^y + 1 => valeur de mcr à partir de y = log10(mcr - 1)
+    }) +
+    ggplot2::theme_bw()
+  
+  # Si des couleurs spécifiques doivent être associés aux THQ
+  if (!is.null(thq_col)) chart = chart + ggplot2::scale_color_manual(values = thq_col)
+  
+  # Texte relatif aux groupes
+  if (any(regions_lab)) {
+    # Vérification des zones affichées (non-affichage du texte des zones qui ne sont pas affichées)
+    regions_lab = regions_lab & c(xlim[2] > 0, xlim[1] < 0, ylim[1] < 0, ylim[2] > 0)
+    
+    chart = chart + ggplot2::annotate(geom = "text",
+                                      x = c(xlim[2], xlim[1], root_fun / 2, xlim[2] / 2)[regions_lab],
+                                      y = c(ylim[1], ylim[1], -0.25, ylim[2])[regions_lab],
+                                      hjust = c(1, 0, 0.5, 0.5)[regions_lab],
+                                      vjust = c(0, 0, 0.5, 1)[regions_lab],
+                                      label = c("Group I", "Group II", "Group IIIA", "Group IIIB")[regions_lab])
+  }
+  
+  # Régression linéaire
+  if (regression) chart = chart + ggplot2::geom_smooth(method = "lm", formula = y ~ x)
+  
+  # Recadrage du graphique
+  chart = chart + ggplot2::coord_cartesian(xlim = xlim, ylim = ylim)
+  
+  graphics::plot(chart)
+}
+
+
--- R/spectral_analyzer.R
+++ R/spectral_analyzer.R
@@ -225,7 +225,7 @@ spectral.analyzer = function(observations, items = NULL, target = "closed freque
   
   # Installation des packages nécessaires au fonctionnement
   # Utile uniquement si les fonctions sont chargées sans charger le package (mode dev)
-  packages = c("arules", "ggsci", "graphics", "grDevices", "mathjaxr", "methods", "network", "sna", "stats", "utils")
+  packages = c("arules", "ggplot2", "ggsci", "graphics", "grDevices", "mathjaxr", "methods", "network", "sna", "stats", "utils")
   new_packages = packages[!(packages %in% utils::installed.packages()[, "Package"])]
   
   if(length(new_packages) != 0) { 
