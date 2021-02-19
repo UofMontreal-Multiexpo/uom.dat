ce5e0b6938975e0e59f0f6924d465d4a54054548
Gauthier_Magnin <gauthier.magnin@gmail.com>
2021-02-19 17:02:05 +0100

    fix: Correct factor to character coercion
    Correct coercion of list of factors to lis of characters while making
    transactions via the function make_transactions.

--- R/transaction_maker.R
+++ R/transaction_maker.R
@@ -87,7 +87,7 @@ make_transactions = function(data, by, additional = NULL, unique_values = TRUE,
   list_cols = names(which(sapply(trx, class) == "list"))
   if (length(list_cols > 0)) {
     if (length(list_cols) == 1) {
-      factor_cols = if (trx[, list_cols][[1]] == "factor") list_cols else NULL
+      factor_cols = if (class(trx[, list_cols][[1]]) == "factor") list_cols else NULL
     } else {
       factor_cols = list_cols[which(sapply(trx[, list_cols], function(x) class(x[[1]])) == "factor")]
     }
