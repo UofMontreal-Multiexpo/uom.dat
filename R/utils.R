8bfe51a56f293734c3296eae2b9982de2c225dc6
Gauthier_Magnin <gauthier.magnin@gmail.com>
2021-03-04 21:14:49 +0100

    docs: Remove an irrelevant reference

--- R/utils.R
+++ R/utils.R
@@ -11,8 +11,6 @@
 #' @param t numeric. Time to convert, in seconds.
 #' @return Character string corresponding to the converted time.
 #' 
-#' @references Stack Overflow topic:
-#'  \href{https://stackoverflow.com/questions/27312292/convert-seconds-to-days-hoursminutesseconds}{Convert seconds to days:hours:minutes:seconds}
 #' @keywords internal
 dhms = function(t){
   t = round(t)
