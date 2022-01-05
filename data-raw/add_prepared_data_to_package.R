f7b0612fce3e21548a8226f95d41f3348ed050f2
Gauthier Magnin <gauthier.magnin@gmail.com>
2022-01-05 19:46:53 +0100

    docs: Translate internal comments of a function

--- data-raw/add_prepared_data_to_package.R
+++ data-raw/add_prepared_data_to_package.R
@@ -26,38 +26,36 @@
 #' @keywords internal
 resave_with_best_compression = function(paths){
   
-  # Vérification de l'existence du package tools (inclus dans DESCRIPTION.Suggests)
+  # Checking the existence of the package tools (included in field "Suggests" of the DESCRIPTION file)
   if (!requireNamespace("tools", quietly = TRUE)) {
     stop("Package \"tools\" needed for this function to work. Please install it.",
          call. = FALSE)
   }
   
-  
-  # Méthodes de compression
+  # Compression methods
   methods = c("gzip", "bzip2", "xz")
   
-  # Pour chaque fichier
+  # For each file
   for (p in paths) {
     
-    # Pour chaque méthode de compression
+    # For each compression method, compress the data and get the file size
     sizes = sapply(methods, function(m) {
-      # Compresse les données et retourne la taille du fichier
       tools::resaveRdaFiles(p, compress = m, compression_level = 9)
       return(tools::checkRdaFiles(p)$size)
     })
     names(sizes) = methods
     
-    # Sélection de la meilleure méthode de compression
+    # Selecting the best compression method
     best = methods[which.min(sizes)]
     
-    # Affichage des résultats et du choix optimal
+    # Display of results and optimal choice
     if (length(paths) != 1) cat("File:", p,"\n")
     cat("File sizes according to compression method:\n")
     print(sizes)
     cat("Use of '", best, "' compression method.\n", sep = "")
     if (p != paths[length(paths)]) cat("\n")
     
-    # Recompression selon la meilleur méthode
+    # Compress again using the best method
     tools::resaveRdaFiles(p, compress = best, compression_level = 9)
   }
 }
